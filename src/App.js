b0ce744a9789d90ea21a9c985d94fc3cafea84e2
Siddharth Ruria <siddharthruria10@gmail.com>
2024-09-17 09:05:16 +0530

    setup default navbar and basic routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <h2>welcome to iNotebook</h2>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>welcome to iNotebook</h2>
+    </div>
+  );
+};
+
+export default Home;
